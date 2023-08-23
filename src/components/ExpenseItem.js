5cc564ef786bb07aef3e299659009b27b9e2d8e4
RehazIsmail <142209667+RehazIsmail@users.noreply.github.com>
2023-08-23 16:22:23 +0200

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,1 +1,24 @@
+const increaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
 
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense
+        });
+
+    }
+
+    return (
+        <tr>
+        <td>{props.name}</td>
+        <td>£{props.cost}</td>
+        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+        </tr>
+    );
+};
+
+export default ExpenseItem;
